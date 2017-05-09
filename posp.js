32e4d6e66e3cfa6273eb1b65c38cb5087158000b
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-09 17:31:18 +0800

    Improved code.

--- posp.js
+++ posp.js
@@ -34,6 +34,9 @@
 			"file": "posp.js",
 			"module": "posp",
 			"author": "Richeve S. Bebedor",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+			],
 			"eMail": "richeve.bebedor@gmail.com",
 			"repository": "https://github.com/volkovasystems/posp.git",
 			"test": "posp-test.js",
