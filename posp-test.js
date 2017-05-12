eb28826d1db91a04a67c5a476f3e41d5a3eb160b
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-12 12:36:02 +0800

    Updated test.

--- posp-test.js
+++ posp-test.js
@@ -1,6 +1,7 @@
 const assert = require( "assert" );
 const posp = require( "./posp.js" );
 
-assert.deepEqual( posp( [ "hello", "world", 1, 2, 3, true, false, 5, 6 ], NUMBER ), [ 'hello', 'world', true, false ] );
+assert.deepEqual( posp( [ "hello", "world", 1, 2, 3, true, false, 5, 6 ],
+NUMBER ), [ 'hello', 'world', true, false ] );
 
 console.log( "ok" );
